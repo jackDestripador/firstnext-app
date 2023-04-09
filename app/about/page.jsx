788490fb3edb37bad8691d659491decb5307fdcc
Jesús Cisneros <chucho.cisneros@hotmail.com>
2023-04-08 18:34:50 -0600

    link

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutPage(){
+  return (
+    <div>
+      <h1> About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore dicta culpa suscipit voluptatum a corrupti voluptates accusamus sapiente praesentium laboriosam eligendi deserunt at repellendus, aperiam voluptatibus nemo? Adipisci, quibusdam suscipit.</p>
+    </div>
+  )
+}
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServicesPage(){
+  return (
+    <div>
+      <h1> Services</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid ad, aliquam at est fugit deserunt similique eum adipisci harum ratione dolore, debitis exercitationem maxime dignissimos assumenda! Eveniet laborum quaerat saepe fugit vel rerum aut ut aspernatur? Nobis ipsa blanditiis nemo eius beatae magnam, explicabo quibusdam, dolore exercitationem quaerat odit.</p>
+    </div>
+  )
+}
+export default ServicesPage
